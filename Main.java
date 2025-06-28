12c48e03f399afee55a764cd6c780be765054dda
CaiusPelish <caius.pelish@gmail.com>
2025-06-28 10:39:17 -0400

    File name changed to Main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void hello() {
+        System.out.println("Hello World");
+    }
+    
+    public static void main(String[] args){
+        hello();
+    }
+}
