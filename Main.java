a5337b5cca59c359e1e87405a267a9d554963b50
CaiusPelish <caius.pelish@gmail.com>
2025-06-28 12:11:58 -0400

    IDK won't let me not commit
    It doesn't really do anything dw

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main {
-    public static void hello() {
+    public static int hello() {
         System.out.println("Hello World");
         int balls = 1;
+        return balls;
     }
     
     public static void main(String[] args){
-        hello();
+        int ballNum = hello();
         //hoy
         int x = 20;
+        System.out.println(x - ballNum); // 20 - 1, prints 19
     }
 }
